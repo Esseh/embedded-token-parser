37e17ce0084d82e28b54ccde9c2a7dfbe2a2af1b
Esseh <kennethwilleford@gmail.com>
2018-05-20 14:38:17 -0700

    Init

---
+++ Makefile
@@ -0,0 +1,4 @@
+BOARD_TAG = uno
+ARDUINO_DIR = /usr/share/arduino
+ARDUINO_LIBS = FastLED Adafruit_NeoPixel
+include $(ARDMK_DIR)/Arduino.mk
---
+++ main.cpp
@@ -0,0 +1,20 @@
+/// Barr Standard Reminder: Item Order
+/// Comment Block
+/// Includes
+/// Datatypes
+/// Constants
+/// Macros
+/// Static Declarations
+/// Private Prototypes
+/// Public Function Bodies
+/// Private Function Bodies
+
+#include<Arduino.h>
+void setup()
+{
+
+}
+void loop()
+{
+
+}
